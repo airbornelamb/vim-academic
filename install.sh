fbe2cebf0136704fef509691dd23d17cb22d0c47
airbornelamb <joshua.lamb@prts.edu>
2017-10-19 16:42:00 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
-echo "runtime vimrc" > ~/.vimrc && mkdir ~/.vim && cd ~/.vim && git clone https://github.com/airbornelamb/vim-starter.git . && git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim && vim +PluginInstall
+sudo apt remove vim*
+sudo apt install vim-nox-py2 git wget
+echo "runtime vimrc" > ~/.vimrc
+mkdir ~/.vim
+cd ~/.vim
+wget https://raw.githubusercontent.com/airbornelamb/vim-starter/master/vimrc
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall
