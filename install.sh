da352d925c3a4e894771b820756efb6ebc51f4cd
airbornelamb <joshua.lamb@prts.edu>
2017-10-19 00:13:24 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,1 @@
+echo "runtime vimrc" > ~/.vimrc && mkdir ~/.vim && cd ~/.vim && git clone https://github.com/airbornelamb/vim-starter.git . && git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim && vim +PluginInstall
