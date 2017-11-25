c59ef559a3b465dd6ffb2b97023a8b6fbb78a399
airbornelamb <joshua.lamb@prts.edu>
2017-11-25 10:11:57 -0500

    Update install.sh
    Updated for Fedora

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 sudo apt update
 sudo apt remove vim*
-sudo apt install -y vim-nox-py2 git pandoc-citeproc
+sudo apt install -y vim-nox-py2 git pandoc pandoc-citeproc
+
+sudo dnf install vim-enhanced git pandoc pandoc-citeproc
+
 echo "runtime vimrc" > ~/.vimrc
 mkdir ~/.vim
-cd ~/.vim
 git clone https://github.com/airbornelamb/vim-academic.git ~/vim-academic
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ~/vim-academic/vimrc ~/.vim/vimrc
