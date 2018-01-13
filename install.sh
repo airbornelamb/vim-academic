afff7a3f47030f605ba49002682103cd92d38ca8
airbornelamb <joshua.lamb@prts.edu>
2018-01-13 09:56:15 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,3 +11,4 @@ git clone https://github.com/airbornelamb/vim-academic.git ~/vim-academic
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ~/vim-academic/vimrc ~/.vim/vimrc
 vim
+cat ./bashrcappend >> ~/.bashrc
