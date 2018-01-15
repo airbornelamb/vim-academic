640731d459cc1a88b8516dbd238714ce900f7c7b
airbornelamb <joshua.lamb@prts.edu>
2018-01-15 07:43:55 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
 sudo apt update
 sudo apt remove vim*
-sudo apt install -y vim-nox-py2 git pandoc pandoc-citeproc highlight
+sudo apt install -y vim-nox-py2 git pandoc pandoc-citeproc ranger
 
 sudo dnf -y update
-sudo dnf -y install vim-enhanced git pandoc pandoc-citeproc highlight
+sudo dnf -y install vim-enhanced git pandoc pandoc-citeproc ranger
 
 echo "runtime vimrc" > ~/.vimrc
 mkdir ~/.vim
 git clone https://github.com/airbornelamb/vim-academic.git ~/vim-academic
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ~/vim-academic/vimrc ~/.vim/vimrc
 vim
 cat ./bashrcappend >> ~/.bashrc
